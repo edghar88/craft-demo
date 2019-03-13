13fe8410a47e8f20aa4cad905e082dd035e8280f
Edgar <Edgar.Harwood@optiv.com>
2019-03-12 20:59:15 -0600

    fixing filename issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FieldBuilder from './components/fieldBuilder'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import './assets/styles/App.css';
+import './App.css';
 
 library.add(faPlus, faTrashAlt);
 
